8868246e6b51994cae3219f58671f5da9b198367
Noah H. Smith <nsmith@rendergardenmedia.com>
2012-10-31 10:21:42 -0500

    defining spec

--- spec/food_junkie/recipe_spec.rb
+++ spec/food_junkie/recipe_spec.rb
@@ -4,8 +4,22 @@ describe FoodJunkie::Recipe do
 
   let(:recipe) do
     FoodJunkie::Recipe.new <<-END_RECIPE
-      3 eggs and 13 pieces of fish
+      3 eggs and 13 pieces of fish.
+
+      Bake for 30 minutes.
     END_RECIPE
+  end
+
+  it "breaks a recipe into steps" do
+    recipe.steps.count.should eq 2
+  end
+
+  it "extrapolates ingredients" do
+    recipe.steps.count.should eq 2
+  end
+
+  it "summarizes the recipe's total cook and prep time" do
+    recipe.total_time.should eq 30 * 60
   end
 
 end
