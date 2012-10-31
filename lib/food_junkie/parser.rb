77b5f3bbf7617951cb09dd2de391646d87b1ff29
Noah H. Smith <nsmith@rendergardenmedia.com>
2012-10-31 16:01:36 -0500

    deprecation warning

--- lib/food_junkie/parser.rb
+++ lib/food_junkie/parser.rb
@@ -1,6 +1,8 @@
 module FoodJunkie
   class Parser
 
+    warn "[DEPRECATION] `FoodJunkie::Parser` will be removed with no replacement. Start using FoodJunkie::Recipe."
+
     # Load the Treetop grammar, and create a new instance
     # of that parser as a class variable so we don't have to
     # re-create it every time we need to parse a string
