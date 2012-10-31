0c20b392f37f091c3c695b756f75a0649cd92438
Noah H. Smith <nsmith@rendergardenmedia.com>
2012-10-31 11:15:18 -0500

    instantiate with SyntaxNode

--- lib/food_junkie/recipe.rb
+++ lib/food_junkie/recipe.rb
@@ -1,19 +1,28 @@
 module FoodJunkie
   class Recipe
-    def initialize(recipe_string)
-      @parser = Parsers::RecipesParser.new.parse recipe_string
+    def initialize(recipe)
+      case recipe
+      when String
+        @syntax_node = Parsers::RecipesParser.new.parse recipe
+      when FoodJunkie::SyntaxNodes::Recipe
+        @syntax_node = recipe
+      end
+    end
+
+    def syntax_node
+      @syntax_node
     end
 
     def steps
-      @parser.children(FoodJunkie::SyntaxNodes::Step)
+      syntax_node.children(FoodJunkie::SyntaxNodes::Step)
     end
 
     def ingredients
-      @parser.children(FoodJunkie::SyntaxNodes::Ingredient)
+      syntax_node.children(FoodJunkie::SyntaxNodes::Ingredient)
     end
 
     def total_time
-      times = @parser.children(FoodJunkie::SyntaxNodes::Time)
+      times = syntax_node.children(FoodJunkie::SyntaxNodes::Time)
 
       times.inject 0 do |total, time|
         total += time.to_i
--- spec/food_junkie/recipe_spec.rb
+++ spec/food_junkie/recipe_spec.rb
@@ -2,12 +2,23 @@ require 'spec_helper'
 
 describe FoodJunkie::Recipe do
 
-  let(:recipe) do
-    FoodJunkie::Recipe.new <<-END_RECIPE
+  let(:recipe_string) do
+    <<-END_RECIPE
       3 eggs and 13 pieces of fish.
 
       Bake for 30 minutes.
     END_RECIPE
+  end
+
+  let(:recipe) do
+    FoodJunkie::Recipe.new(
+      FoodJunkie::Parsers::RecipesParser.new.parse(recipe_string.to_s)
+    )
+  end
+
+  it "can be initialized with a string" do
+    recipe = FoodJunkie::Recipe.new recipe_string
+    recipe.syntax_node.should be_kind_of FoodJunkie::SyntaxNodes::Recipe
   end
 
   it "breaks a recipe into steps" do
